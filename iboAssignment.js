f233c83070268bf63b4fe9d44631497df1a28931
Rupam Shaw <97508688+rupamShaw1998@users.noreply.github.com>
2022-07-03 13:33:29 +0530

    done

---
+++ iboAssignment.js
@@ -0,0 +1,98 @@
+/*
+You have been given a list of products which is having property productName, quantity and description.
+
+
+PROBLEM STATEMENTS:
+
+1. you need to write a function say, getUniqueProductCount which should return count of each Product(as an object) present in the given list of Products considering Product Name as Key.
+
+Sample Output for the given listOfProducts will be :
+
+{
+  "TV": 2,
+  "AC": 2,
+  "FAN": 1
+}
+
+
+
+2. you need to write a function say, getUniquePrducts which should return an array of objects by grouping the products based on the productName and summing up the quantity for the same products present in the given list of Products considering Product Name as Key.
+
+Sample Output for the given listOfProducts will be :
+
+[{
+    productName: "TV",
+    quantity: 20,
+    description: "television"
+  },
+  {
+    productName: "AC",
+    quantity: 10,
+    description: "air conditioner"
+  },
+  {
+    productName: "FAN",
+    quantity: 10,
+     description: "Ceiling Fan"
+  }
+]
+
+*/
+
+
+
+
+const listOfProducts = [{
+    productName: "TV",
+    quantity: 10,
+    description: "television"
+  },
+  {
+    productName: "AC",
+    quantity: 5,
+    description: "air conditioner"
+  },
+  {
+    productName: "TV",
+    quantity: 10,
+    description: "television"
+  },
+  {
+    productName: "AC",
+    quantity: 5,
+    description: "air conditioner"
+  },
+  {
+    productName: "FAN",
+    quantity: 10,
+    description: "Ceiling Fan"
+  }
+];
+
+function getUniqueProductCount() {
+  const ob = {};
+  listOfProducts.map(e => {
+    if(ob[e.productName]==undefined)
+    	ob[e.productName]=1;
+    else
+    	ob[e.productName]++;
+  })
+  return ob;
+}
+
+console.log(getUniqueProductCount());
+
+function getUniqueProducts() {
+  let obj = {};
+  listOfProducts.map(e => {
+    if(obj[e.productName])
+      obj[e.productName].quantity = obj[e.productName].quantity + e.quantity;
+    else
+      obj[e.productName] = e;
+  })
+  return Object.values(obj);
+}
+
+console.log(getUniqueProducts());
+
+
